cb1d09aca997b21eace375f15c1d5352129770de
hheadake <113627073+hheadake@users.noreply.github.com>
2024-01-03 14:21:16 +0200

    Create SumFirstAndLast.js
    This is a basic summing function that returns the sum of the first and the last element of the array

---
+++ SumFirstAndLast.js
@@ -0,0 +1,16 @@
+function sum(array) {
+
+let firstNum = array[0];
+let lastNum = 0;
+
+for (let index = 0; index < array.length; index++) {
+   lastNum = array[index]
+   
+    
+}
+
+let totalSum = firstNum + lastNum;
+return totalSum;
+
+
+} sum([20, 30, 40])
